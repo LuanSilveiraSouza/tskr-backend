8ad18fd4c9707b6e38450d66521bd3ddb35ff031
LuanSilveiraSouza <luansouzasilveira@gmail.com>
2020-02-10 14:55:58 -0300

    Connecting to database creating .env file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+async function connect() {
+    await mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+        .then(function() {
+            console.log('DB Connection: OK');
+        })
+        .catch(function() {
+            console.log('DB Connection: Failed');
+        });
+}
+
+connect();
 
 app.get('/', function(res, res) {
     res.send('Hello World!!!');
